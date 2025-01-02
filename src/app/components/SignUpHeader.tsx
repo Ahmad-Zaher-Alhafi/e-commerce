b6c34b154fc952b10fa5bad2e100b1c465a0afbb
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2025-01-02 11:52:33 +0300

    UPDATE: Better styling for the sign up header compoenet

--- src/app/components/SignUpHeader.tsx
+++ src/app/components/SignUpHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SignUpHeader = () => {
     return (
-        <div className={"signUpHeader flex justify-between px-[100px] pt-[12px] bg-black"}>
+        <div className={"signUpHeader w-full h-[38px] flex justify-between items-center px-[100px] py-[12px] bg-black "}>
             <div className={"font-satoshi"}>up and get 20% off to your first order. Sign Up Now</div>
             <img src="/assets/svgs/cross.svg" alt="cross icon"/>
         </div>
