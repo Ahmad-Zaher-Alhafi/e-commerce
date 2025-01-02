1f9b613cb18c66870a389fdae982c5f972ae0626
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2025-01-02 14:05:00 +0300

    UPDATE: Responsive design for SignUpHeader

--- src/app/components/SignUpHeader.tsx
+++ src/app/components/SignUpHeader.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const SignUpHeader = () => {
     return (
-        <div className={"signUpHeader w-full h-[38px] flex justify-between items-center px-[100px] py-[12px] bg-black "}>
-            <div className={"font-satoshi"}>up and get 20% off to your first order. Sign Up Now</div>
-            <img src="/assets/svgs/cross.svg" alt="cross icon"/>
+        <div
+            className={"signUpHeader w-full h-[34px] sm:h-[38px] flex justify-between items-center py-[9px] sm:py-[12px] bg-black text-white "}>
+            <div className={"font-satoshi mx-auto text-[12px] sm:text-[14px]"}>Sign up and get 20% off to your first
+                order. Sign Up Now
+            </div>
+            <img src="/assets/svgs/cross.svg" alt="cross icon" className={"hidden sm:block mr-[44px] sm:mr-[100px]"}/>
         </div>
     )
 }
