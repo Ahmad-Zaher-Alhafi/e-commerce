6ed7338b1ab5f1912ce7e2dc723a82cbf8281858
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2025-01-04 22:55:03 +0300

    UPDATE: Replaced img by Image components to increase performance

--- src/app/components/SignUpHeader.tsx
+++ src/app/components/SignUpHeader.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const SignUpHeader = () => {
-    return (
-        <div
-            className={"signUpHeader w-full h-[34px] sm:h-[38px] flex justify-between items-center py-[9px] sm:py-[12px] bg-black text-white "}>
-            <div className={"font-satoshi mx-auto text-[12px] sm:text-[14px]"}>Sign up and get 20% off to your first
-                order. Sign Up Now
-            </div>
-            <img src="/assets/svgs/cross.svg" alt="cross icon" className={"hidden sm:block mr-[44px] sm:mr-[100px]"}/>
-        </div>
-    )
-}
-export default SignUpHeader
+  return (
+    <div
+      className={
+        "signUpHeader w-full h-[34px] sm:h-[38px] flex justify-between items-center py-[9px] sm:py-[12px] bg-black text-white "
+      }
+    >
+      <div className={"font-satoshi mx-auto text-[12px] sm:text-[14px]"}>
+        Sign up and get 20% off to your first order. Sign Up Now
+      </div>
+      <Image
+        src="/assets/svgs/cross.svg"
+        alt="cross icon"
+        className={"hidden sm:block mr-[44px] sm:mr-[100px]"}
+      />
+    </div>
+  );
+};
+export default SignUpHeader;
