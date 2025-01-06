44d631c708bcb8951d087f2b997555d2868c6d4c
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2025-01-06 17:00:47 +0300

    REMOVE: A useless delay in the product controller

--- DB/productController.ts
+++ DB/productController.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-
   try {
     return await prisma.product.findMany();
   } catch (error) {
